98ce6fcda4ba31f520bc438e707941bf0d4034a7
Devjyoti03 <banerjeedevjyoti7@gmail.com>
2024-06-26 09:22:41 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,25 @@
+# Simple Calculator
 from nada_dsl import *
-
 
 def nada_main():
     party1 = Party(name="Party1")
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    operation = Input(name="operation", party=party1)  # Operation input (add, subtract, multiply, divide)
 
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
+    # Perform the calculation based on the operation input
+    result = SecretInteger()
 
-    return [Output(my_int1, "my_output", party1)]
+    with If(operation == "add"):
+        result = my_int1 + my_int2
+    with ElseIf(operation == "subtract"):
+        result = my_int1 - my_int2
+    with ElseIf(operation == "multiply"):
+        result = my_int1 * my_int2
+    with ElseIf(operation == "divide"):
+        result = my_int1 / my_int2
+    with Else():
+        result = SecretInteger(0)  # Default to 0 if the operation is not recognized
+
+    # Output the result
+    return [Output(result, "result", party1)]
